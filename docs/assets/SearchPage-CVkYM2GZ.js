3d0cfc3c092ad9f72f15822b281b8abb80874812
tatancarduar <tatancarduar@hotmail.com>
2025-11-01 17:30:15 -0500

    second commit

--- docs/assets/SearchPage-CVkYM2GZ.js
+++ docs/assets/SearchPage-CVkYM2GZ.js
@@ -1,4 +1,4 @@
-import{c as H,j as n,a as D,r as u,u as de,b as L,d as ee,e as ue,f as P,P as R,g as _,h as fe,i as Ue,k as me,l as qe,R as A,m as pe,B,n as Ge,o as Ye,C as We,p as Xe,H as Qe,q as Je}from"./index-C2JYeOys.js";/**
+import{c as H,j as n,a as D,r as u,u as de,b as L,d as ee,e as ue,f as P,P as R,g as _,h as fe,i as Ue,k as me,l as qe,R as A,m as pe,B,n as Ge,o as Ye,C as We,p as Xe,H as Qe,q as Je}from"./index-CUaQ4fVx.js";/**
  * @license lucide-react v0.544.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-})
+	base: "/heroes-frontend/", // 👈 MUY IMPORTANTE (nombre exacto de tu repo en GitHub)
+	plugins: [react(), tailwindcss(), tsconfigPaths()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+	},
+});
